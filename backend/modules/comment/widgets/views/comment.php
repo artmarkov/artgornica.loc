07fa4f40c173494ef68c220444e7ceef96c92c94
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-02-20 17:53:24 +0300

    refactor view comment frontend

--- backend/modules/comment/widgets/views/comment.php
+++ backend/modules/comment/widgets/views/comment.php
@@ -11,20 +11,24 @@ Comments::getInstance()->commentsAssetUrl = $commentsAsset->baseUrl;
 ?>
 
 <div class="clearfix recent-comment">
-    <div class="pull-left">
-        <small class="fsize13">
-            <img class="avatar" src="<?= Comments::getInstance()->renderUserAvatar($comment->user_id) ?>"/>
-            <span class="scrollTo label label-success light"><i class="fa fa-comment-o" aria-hidden="true"></i>
-                <?= Html::encode($comment->getAuthor()); ?> <?= "{$comment->createdDate} {$comment->createdTime}" ?>
-            </span>
-        </small>
-        <div>
-            <p>            
+    <div class="pull-left">     
+        <div class="avatar">
+            <img src="<?= Comments::getInstance()->renderUserAvatar($comment->user_id) ?>"/>
+        </div>
+        <div class="content">
+            <div class="header">
+                <span class="scrollTo label label-info light"><i class="fa fa-user" aria-hidden="true"></i>
+                    <?= Html::encode($comment->getAuthor()); ?> <?= "{$comment->createdDate} {$comment->createdTime}" ?>
+                </span>
+            </div>           
+            <div class="text">
+
                 <?= $comment->shortContent ?>
                 <br />
-                 <?= Html::a('<i class="fa fa-sign-out"></i><span class="uppercase">' . Yii::t('yee', 'Read more...') . '</span>', ["/site{$comment->url}"], ['class' => 'btn btn-xs']) ?>
-               
-            </p> 
+                <?= Html::a('<i class="fa fa-sign-out"></i><span class="uppercase">' . Yii::t('yee', 'Read more...') . '</span>', ["/site{$comment->url}"], ['class' => 'btn btn-xs btn-comment']) ?>
+
+            </div>
         </div>
     </div>
 </div>
+
